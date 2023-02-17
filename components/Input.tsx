ae2fd1c7f659f2d077160d142915344d64ba6650
march213 <jane.molodetskaya@gmail.com>
2023-02-16 22:23:37 -0500

    add Button, Card and Input components

---
+++ components/Input.tsx
@@ -0,0 +1,14 @@
+import clsx from 'clsx';
+
+export interface InputProps extends React.HTMLAttributes<HTMLInputElement> {
+  className?: string;
+}
+
+export const Input = ({ className, ...props }: InputProps) => {
+  return (
+    <input
+      className={clsx('border-solid border-gray border-2 px-6 py-2 text-lg rounded-3xl w-full', className)}
+      {...props}
+    />
+  );
+};
