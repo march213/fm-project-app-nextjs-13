35d7d339ba448c517c4506f8f5422ae48028cab0
march213 <jane.molodetskaya@gmail.com>
2023-02-20 14:43:02 -0500

    update home page to have a greetings component and use suspense

--- app/(dashboard)/home/page.tsx
+++ app/(dashboard)/home/page.tsx
@@ -1,5 +1,29 @@
-const Home = () => {
-  return <div>Home</div>;
-};
+import { Greetings } from '@/components/Greetings';
+import { GreetingsSkeleton } from '@/components/GreetingsSkeleton';
+import Link from 'next/link';
+import { Suspense } from 'react';
+import { cookies } from 'next/headers';
+import { db } from '@/lib/db';
+import { delay } from '@/lib/async';
+import { getUserFromCookie } from '@/lib/auth';
 
-export default Home;
+export default async function Page() {
+  return (
+    <div className="h-full overflow-y-auto pr-6 w-1/1">
+      <div className=" h-full  items-stretch justify-center min-h-[content]">
+        <div className="flex-1 grow flex">
+          <Suspense fallback={<GreetingsSkeleton />}>
+            <Greetings />
+          </Suspense>
+        </div>
+        <div className="flex flex-2 grow items-center flex-wrap mt-3 -m-3 ">
+          {/** projects map here */}
+          <div className="w-1/3 p-3">{/* new project here */}</div>
+        </div>
+        <div className="mt-6 flex-2 grow w-full flex">
+          <div className="w-full">{/* tasks here */}</div>
+        </div>
+      </div>
+    </div>
+  );
+}
